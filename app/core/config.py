b5e0369d5204f5ac319968ef0877c22483087941
oubes <omargamal.bs99@gmail.com>
2026-03-12 16:38:17 +0200

    updating the config

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,17 @@
 import ast
+import torch
 
 # =========================
 # Paths & Model Configuration
 # =========================
+
 DATA_PATH = r"data/sales_dataset.xlsx"
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
 MAX_NEW_TOKENS = 128
 
 MODEL_NAME = "qwen-plus"
 API_KEY_ENV_VAR = "DASHSCOPE_API_KEY"
 BASE_URL = "https://dashscope-intl.aliyuncs.com/compatible-mode/v1"
+
+MAX_RETRIES = 5
