745c038e5120dd8d0d11de4468e2fefb785be61d
oubes <omargamal.bs99@gmail.com>
2026-03-13 01:19:06 +0200

    adding the auto generation system graph of langgraph code

--- scripts/graph_utils.py
+++ scripts/graph_utils.py
@@ -3,11 +3,12 @@ from langgraph.graph import StateGraph
 
 def save_graph_image(graph: StateGraph, filename: str = "graph_flow.png"):
     try:
-        project_root = Path(__file__).resolve().parents[2]
+        project_root = Path(__file__).resolve().parents[1]
         save_dir = project_root / "app" / "data"
         save_dir.mkdir(parents=True, exist_ok=True)
         full_path = save_dir / filename
         img_data = graph.get_graph().draw_mermaid_png()  # type: ignore
+        print(f"Saving graph image to: {full_path}")
         with open(full_path, "wb") as f:
             f.write(img_data)
     except Exception:
